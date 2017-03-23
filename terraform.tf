08dadcb0c35438d63d401fafaaf9ebaaf9247698
ambarish2012 <ambarish@shippable.com>
2017-03-23 11:35:48 -0700

    Update pipeline with resource changes

--- terraform.tf
+++ terraform.tf
@@ -4,13 +4,13 @@ provider "aws" {}
 
 # EC2 instances
 resource "aws_instance" "sampleInstances" {
-  count = 2
+  count = 1
 
   # ami = "${var.ecsAmi}"
-  ami = "ami-0d729a60"
-  availability_zone = "us-east-1b"   #REPLACE THIS WITH YOUR AVAILABILITY ZONE
+  ami = "ami-79d45d19" #This has to be an EBS backed AMI and not a S3 backed AMI
+  availability_zone = "us-west-2b"   #REPLACE THIS WITH YOUR AVAILABILITY ZONE
   instance_type = "t2.micro"
-  subnet_id = "subnet-5bfa2e76"  #REPLACE THIS WITH YOUR SUBNET ID
+  subnet_id = "subnet-f320be94"  #REPLACE THIS WITH YOUR SUBNET ID
   associate_public_ip_address = true
   source_dest_check = false
 
